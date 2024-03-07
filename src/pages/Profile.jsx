e2b65e96d189c7cf1f0e0e9564cf75164802519f
Matija Skrletović <skrlama@gmail.com>
2024-03-07 22:02:10 +0100

    private route with custom hook for protecting profile page and add the logout funncinality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { getAuth } from 'firebase/auth';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router';
 
 export default function Profile() {
+
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  });
+
+  const {name, email} = formData;
+
+  async function onLogout() {
+
+    auth.signOut();
+    
+    navigate("/");
+
+  }
+
   return (
-    <div>
-      Profile
-    </div>
+    <>
+      <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
+        <h1 className='text-3xl text-center mt-6 font-bold'>
+          My Profile
+        </h1>
+        <div className='w-full md:w-[50%] mt-6 px-3'>
+          <form>
+            <input type="text" id='name' value={name} 
+              disabled className='w-full px-4 py-2 text-xl text-gray-700 
+            bg-white border-gray-300 rounded transition ease-in-out mb-6' />
+
+            <input type="email" id='email' value={email} 
+              disabled className='w-full px-4 py-2 text-xl text-gray-700 
+            bg-white border-gray-300 rounded transition ease-in-out mb-6' />
+
+            <div className='flex justify-between mb-6 whitespace-nowrap text-sm sm:text-lg'>
+              <p className='flex items-center'>Do you want to change your name?
+                <span className='text-red-600 hover:to-red-700 
+                transition duration-200 ease-in-out ml-1 cursor-pointer'>
+                  Edit
+                </span>
+              </p>
+              <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>
+                Sing out
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   )
 }
