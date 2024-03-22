773622e2d4d1bd7f09c5828fe40f962e447ad7ef
Matija Skrletović <skrlama@gmail.com>
2024-03-22 22:46:26 +0100

    Added listing information to the listing page

---
+++ src/components/listing/ListingIcons.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ListingIcons({children}) {
+  return (
+    <li className='flex items-center whitespace-nowrap'>
+        {children}
+    </li>
+  )
+}
