4d141e51d2d56dbfac7756c6aca5b56cb78ec97c
Matija Skrletović <skrlama@gmail.com>
2024-03-22 21:24:27 +0100

    added share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,6 +6,7 @@ import Spinner from '../components/Spinner';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css/bundle";
+import { FaShare } from 'react-icons/fa'
 
 
 export default function Listing() {
@@ -13,6 +14,7 @@ export default function Listing() {
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
     useEffect(()=> {
         async function fetchListing() {
@@ -34,7 +36,7 @@ export default function Listing() {
             <main>
                 <Swiper modules={[EffectFade, Autoplay, Navigation, Pagination]} slidesPerView={1} 
                 navigation pagination={{type: "progressbar"}} effect="fade"
-                 autoplay={{ delay: 10000}} >
+                autoplay={{ delay: 10000}} >
                     {listing.imgUrls.map((url, index)=>(
                         <SwiperSlide key={index}>
                             <div className='relative w-full overflow-hidden h-[300px]' 
@@ -45,6 +47,21 @@ export default function Listing() {
                         </SwiperSlide>
                     ))}
                 </Swiper>
+                <div className='fixed top-[10%] right-[5%] z-10 
+                bg-white cursor-pointer border-2 border-gray-400 rounded-full
+                w-12 h-12 flex justify-center items-center' onClick={() =>{
+                    navigator.clipboard.writeText(window.location.href);
+                    setShareLinkCopied(true);
+                    setTimeout(() => {
+                        setShareLinkCopied(false);
+                    }, 2000)
+                }}>
+                    <FaShare className='text-lg text-slate-500' />
+                </div>
+                { shareLinkCopied && <p className='fixed top-[16%] right-[3%] 
+                font-semibold border-2 border-gray-400 rounded-md bg-white z-10'>
+                    Link Copied!
+                </p>}
             </main>
         )
     }
